b8c5fbcac0956fc1be680259bed08ef256578915
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-06-04 10:51:32 +0300

    create folder sync, create test for nativenull func

--- 01_intro/intro.test.js
+++ 01_intro/intro.test.js
@@ -1,8 +1,33 @@
-const {sum} = require('./intro')
+const {sum, nativeNull} = require('./intro')
 
-
-// в callback функции описываем тест; toBe и toEqual это "мэтчеры"
-test('Sum should return sum of two values', () => {
-  expect(sum(1,2)).toBe(3)
-  expect(sum(1,2)).toEqual(3)
+describe('Sum function:', () => {
+  // в callback функции описываем тест; toBe и toEqual это "мэтчеры"
+test('should return sum of two values', () => {
+  expect(sum(1, 2)).toBe(3)
+  expect(sum(1, 2)).toEqual(3)
 })
+
+test('should return sum value correctly comparing to other valuse', () => {
+  expect(sum(2, 3)).toBeGreaterThan(4)
+  expect(sum(2, 3)).toBeGreaterThanOrEqual(5)
+  expect(sum(2, 3)).toBeLessThan(7)
+  expect(sum(2, 3)).toBeLessThanOrEqual(5)
+})
+
+test('should return sum of two float values correctly', () => {
+  // expect(sum(0.1, 0.2)).toBe(0.3) тест не пройдет - из-за округления в js; используем другой матчер
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3)
+})
+})
+
+describe('Native null function:', () => {
+  test('should return null value', () => {
+    expect(nativeNull()).toBe(null)
+    expect(nativeNull()).toBeNull()
+    expect(nativeNull()).toBeFalsy()
+    expect(nativeNull()).toBeDefined()
+    expect(nativeNull()).not.toBeTruthy()
+    expect(nativeNull()).not.toBeUndefined()
+  })
+})
+
